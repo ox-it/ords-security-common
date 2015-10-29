d7d4cfdd4c0f993a5a9ed52a3681daecd3742a04
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-10-29 21:06:58 +0000

    Put more checks in around session invalidation and session flooding

--- src/main/java/uk/ac/ox/it/ords/security/SSOFilter.java
+++ src/main/java/uk/ac/ox/it/ords/security/SSOFilter.java
@@ -20,6 +20,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
@@ -29,7 +30,15 @@ public class SSOFilter extends AuthenticatingFilter {
 	@Override
 	protected AuthenticationToken createToken(ServletRequest request,
 			ServletResponse response) throws Exception {	
-		String user = ((HttpServletRequest)request).getRemoteUser();
+		String user;
+		try {
+			user = ((HttpServletRequest)request).getRemoteUser();
+		} catch (Exception e) {
+			//
+			// No current HTTPSession
+			//
+			user = null;
+		}
 		String affiliation = (String)request.getAttribute("affiliation");
 		if (affiliation == null) affiliation = "";
 		RemoteUserToken token = new RemoteUserToken(user, affiliation);
@@ -39,27 +48,55 @@ public class SSOFilter extends AuthenticatingFilter {
 	@Override
 	protected boolean isAccessAllowed(ServletRequest request,
 			ServletResponse response, Object mappedValue) {
+		
+		Subject subject = getSubject(request, response);
+
 		try {
-			AuthenticationToken token = createToken(request, response);
-			Subject subject = getSubject(request, response);
 			//
 			// We have to logout first to ensure stateless operation, as 
 			// even with session persistence off we still retain the 
-			// same principal if we call login()
+			// same principal if we call login(). We do this
+			// with the anon user as they MUST be stateless.
 			//
-			subject.logout();
+			if (subject.getPrincipal()!=null && subject.getPrincipal().equals("anonymous")){
+				subject.logout();
+			}
+		} catch (Exception e1) {
+			//
+			// This shouldn't happen ... 
+			// TODO enable proper logging
+			//
+			e1.printStackTrace();
+		}
+		
+		//
+		// Login with current REMOTE_USER
+		//
+		try {
+			AuthenticationToken token = createToken(request, response);
 			subject.login(token);
 			if (subject.isAuthenticated()){
 				return true;
 			};
+		} catch (AuthenticationException e) {
+			//
+			// In Permissive mode, create an anonymous subject
+			//
+			// We have to "login" with anonymous to allow anonymous role permission
+			// checking
+			//
+			if (isPermissive(mappedValue)){
+				RemoteUserToken token = new RemoteUserToken("anonymous", "");
+				subject.login(token);
+				return true;
+			}			
 		} catch (Exception e) {
-		}
-		if (isPermissive(mappedValue)){
-			RemoteUserToken token = new RemoteUserToken("anonymous", "");
-			Subject subject = getSubject(request, response);
-			subject.logout();
-			subject.login(token);
-			return true;
+			//
+			// Something went wrong, so lets assume no login is possible at
+			// the moment.
+			// TODO enable proper logging
+			e.printStackTrace();
+			return false;
 		}
 		return false;
 	}
