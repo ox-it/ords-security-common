878d23e517b027647d583b5d15e30aca8a3693af
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-11-06 15:15:52 +0000

    Added Shiro HMAC implementation

---
+++ src/main/java/uk/ac/ox/it/ords/security/HMACToken.java
@@ -0,0 +1,33 @@
+package uk.ac.ox.it.ords.security;
+
+import org.apache.shiro.authc.AuthenticationToken;
+
+public class HMACToken implements AuthenticationToken {
+
+	private Object principal;
+	private Object credentials;
+	
+	public HMACToken(){
+	}
+	
+	public HMACToken(Object principal, Object credentials){
+		this.principal = principal;
+		this.credentials = credentials;
+	}
+
+	public Object getPrincipal() {
+		return principal;
+	}
+
+	public Object getCredentials() {
+		return credentials;
+	}
+
+	public void setPrincipal(Object principal) {
+		this.principal = principal;
+	}
+
+	public void setCredentials(Object credentials) {
+		this.credentials = credentials;
+	}
+}
